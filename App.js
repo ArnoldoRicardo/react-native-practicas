fcf59f7f9819975929e1657c14fb648dcf267038
ArnoldoRicardo <anyback@gmail.com>
2021-05-21 16:28:24 -0500

    list

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import axios from 'axios';
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () =>{
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+      setLoading(false)
+      setUsers(await response.data)
+    }
+    getData()
+  },[]);
+  
+  
+  if (loading) {
+    return <View style={styles.center}><Text>Cargadando..</Text></View>
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <FlatList
+      data={users}
+      renderItem={({item})=> <Text style={styles.item}>{item.name}</Text>}
+      keyExtractor={item => String(item.id)}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  center: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "center",
+    paddingTop: 22,
   },
 });
